0b40f26a4cab05cbec97a3c5f7defcfa6224c382
mike-jones <michaeldeitch04@gmail.com>
2025-10-29 17:48:25 -0700

    Basic implementatino for type checking via unification algorithm
    By implementing the Moller algorithm for unification, based on the
    union-find data structure, unify is now able to report if there are
    any type errors within the function of the provided file.
    
    Small note:
    Deviations in the algorithm exist on line 41, I believe that my implementation
    does not require looking at sub terms (mostly since there are no subterms).

--- Unify.py
+++ Unify.py
@@ -8,31 +8,66 @@
 
 import pycparser
 import Constrain
+from Constrain import IDTerm, TypeTerm
 
 
-class Node:
-    def __init__(self, value):
-        self.value = value
+def _make_set(term):
+    term.parent = term
 
+def find(term):
+    if term.parent != term:
+        term.parent = find(term.parent)
 
-def nodify_constraints(filename):
-    #Generate the constraint system for the file (function)
+    return term.parent
+
+def union(term_x, term_y):
+    x_representative = find(term_x)
+    y_representative = find(term_y)
+
+    if x_representative != y_representative:
+        x_representative.parent = y_representative
+
+def unify(term_1, term_2):
+    term_1_representative = find(term_1)
+    term_2_representative = find(term_2)
+
+    if term_1_representative != term_2_representative:
+        if isinstance(term_1_representative, IDTerm) and isinstance(term_2_representative, IDTerm):
+            union(term_1_representative, term_2_representative)
+        elif isinstance(term_1_representative, IDTerm) and isinstance(term_2_representative, TypeTerm):
+            union(term_1_representative, term_2_representative)
+        elif isinstance(term_1_representative, TypeTerm) and isinstance(term_2_representative, IDTerm):
+            union(term_2_representative, term_1_representative)
+        elif isinstance(term_1_representative, TypeTerm) and isinstance(term_2_representative, TypeTerm) and term_1_representative.name == term_2_representative.name:
+            union(term_1_representative, term_2_representative)
+        else:
+            return False
+    return True
+
+def perform_unification(filename):
+    #collect constraint system
     constraints = Constrain.create_constraints(filename)
 
-    #Prepare for storage of each term as a node
-    nodes = []
-
+    #base for unification
     for constraint in constraints:
-        term_l = constraint.l_term
-        term_r = constraint.r_term
+        term_a = constraint.l_term
+        term_b = constraint.r_term
 
-        nodes.append(Node(term_l.name))
-        nodes.append(Node(term_r.name))
+        _make_set(term_a)
+        _make_set(term_b)
 
-    return nodes
+    #attempt to unify each constraint
+    for constraint in constraints:
+        term_a = constraint.l_term
+        term_b = constraint.r_term
+
+        unified = unify(term_a, term_b)
+
+        if not unified:
+            return False
+
+    return True
 
 if __name__ == "__main__":
     filename = "Trivial.c"
-    nodified_constraints = nodify_constraints(filename)
-    x = "stop"
-    print("balls")
+    print(f"\n\n{perform_unification(filename)}")
